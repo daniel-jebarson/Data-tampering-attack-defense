6ca9f1c031beb0bc16fa83edf4c9133a4981a720
daniel-jebarson <danieljebarson21@gmail.com>
2022-10-18 13:25:38 +0530

    added host port 🤩

--- index.js
+++ index.js
@@ -48,6 +48,6 @@ app.post("/create-checkout-session", async (req, res) => {
   }
 });
 const PORT = process.env.PORT || 3000;
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running in ${PORT}`);
 });
